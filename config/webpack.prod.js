a336dd3a21869640b490af3d174be585fb59ba6a
wachsenhaus <bi9choi@gmail.com>
2022-06-20 00:08:26 +0900

    init

---
+++ config/webpack.prod.js
@@ -0,0 +1,48 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: './',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  optimization: {
+    usedExports: true,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+      new CssMinizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+});
